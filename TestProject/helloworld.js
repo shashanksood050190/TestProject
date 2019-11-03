a7e3895a779a5a60620ece9ebae05d804f4edb28
shashanksood050190 <52665192+shashanksood050190@users.noreply.github.com>
2019-11-03 22:23:44 +0530

    Add files via upload

---
+++ TestProject/helloworld.js
@@ -0,0 +1,24 @@
+//single line
+/*
+***
+*/
+
+console.log('hello world');
+
+/*** variables ***/
+
+let x;
+x=10;
+
+let x=10;
+
+let message='Hello';
+message='Welcome';
+
+let x,y;
+x=10;
+
+let x=10,y=20;
+
+
+message='Welcome'
